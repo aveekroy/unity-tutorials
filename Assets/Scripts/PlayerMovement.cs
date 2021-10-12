a16686b715b19a23b16f3b874d7eacac45f4d426
Aveek Roy <aveek.roy@outlook.com>
2021-10-12 00:38:27 -0700

    added if conditions for controlling the player using a and d keys

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,24 @@ public class PlayerMovement : MonoBehaviour
 
     public float forwardForce = 2000f;
 
+    public float sidewaysForce = 500f;
+
+
     // FixedUpdate is called once per frame when physics is involved - it is recommended
     void FixedUpdate()
     {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime,0,0);
+        }
+
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
