2d710d9e2eda10737d48befc524c9562b0915844
Aveek Roy <aveek.roy@outlook.com>
2021-10-12 11:51:46 -0700

    incorporated tag concept so that we know with what we collided and take appropriate action

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -7,6 +7,11 @@ public class PlayerCollision : MonoBehaviour
         // If rigidbody or the box collider is there, this function would be called.
 
         Debug.Log("We hit ->" + collision.collider.name);
+
+        if(collision.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle, Aveek!");
+        }
     }
 
 }
