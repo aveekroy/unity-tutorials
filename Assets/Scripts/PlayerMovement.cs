a03008bd2706f6ccbd64818ec58bfeec9061664a
Aveek Roy <aveek.roy@outlook.com>
2021-10-11 22:24:54 -0700

    added debug log statement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello, Aveek!");
     }
 
     // Update is called once per frame
