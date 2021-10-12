40461aa3913b0614a2f65a4021db811b71a767c4
Aveek Roy <aveek.roy@outlook.com>
2021-10-11 23:12:06 -0700

    added rigidbody to the C# script and made changes to the component's gravity when the game starts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, Aveek!");
+        rb.useGravity = false;
     }
 
     // Update is called once per frame
