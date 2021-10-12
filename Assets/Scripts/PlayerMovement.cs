36063bc16a7fc3423157113ef0510d9a22607ed6
Aveek Roy <aveek.roy@outlook.com>
2021-10-11 23:29:41 -0700

    continuous adding force to the gameobject using Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-        rb.AddForce(0, 200, 500);
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
     }
 }
