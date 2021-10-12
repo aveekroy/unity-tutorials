a8c1c2583fba4408b3897313cb41ab2cd92d54b8
Aveek Roy <aveek.roy@outlook.com>
2021-10-12 16:16:48 -0700

    added Prefabs for Obstacles

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    public Rigidbody rb;
+	// This is a reference to the Rigidbody component called "rb"
+	public Rigidbody rb;
 
-    public float forwardForce = 2000f;
+	public float forwardForce = 2000f;  // Variable that determines the forward force
+	public float sidewaysForce = 500f;  // Variable that determines the sideways force
 
-    public float sidewaysForce = 500f;
+    // We marked this as "Fixed"Update because we
+    // are using it to mess with physics.
 
 
-    // FixedUpdate is called once per frame when physics is involved - it is recommended
     void FixedUpdate()
-    {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+	{
 
+		// Add a forward force
+		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(sidewaysForce * Time.deltaTime,0,0);
-        }
+		if(Input.GetKey("d"))  // If the player is pressing the "d" key
+		{
+			// Add a force to the right
+			Debug.Log("d pressed");
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+		}
 
-
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
-        }
-    }
+		if(Input.GetKey("a"))  // If the player is pressing the "a" key
+		{
+			// Add a force to the left
+			Debug.Log("a pressed");
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+		}
+	}
 }
--- README.md
+++ README.md
@@ -40,3 +40,5 @@ The Toolbar is always at the top of the Unity Editor interface. Use the toolbar 
 > Materials - Applying colors to GameObjects
 
 > Physic Material - used to adjust friction and bouncing effects of colliding objects.
+
+> Prefabs - allows to duplicate gameObjects so that we don't have to create similar things manually.
