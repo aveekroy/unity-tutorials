be215ebbe544ac9b13634a0f004d90a3b95d026a
Aveek Roy <aveek.roy@outlook.com>
2021-10-12 17:24:11 -0700

    added Forcemode and some level setup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,31 +8,33 @@ public class PlayerMovement : MonoBehaviour
 	// This is a reference to the Rigidbody component called "rb"
 	public Rigidbody rb;
 
-	public float forwardForce = 2000f;  // Variable that determines the forward force
+	public float forwardForce = 500f;  // Variable that determines the forward force
 	public float sidewaysForce = 500f;  // Variable that determines the sideways force
 
     // We marked this as "Fixed"Update because we
     // are using it to mess with physics.
 
 
-    void FixedUpdate()
+    private void FixedUpdate()
 	{
 
 		// Add a forward force
 		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-		if(Input.GetKey("d"))  // If the player is pressing the "d" key
+		if (Input.GetKey(KeyCode.D)) // If the player is pressing the "d" key
 		{
 			// Add a force to the right
 			Debug.Log("d pressed");
-			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 
-		if(Input.GetKey("a"))  // If the player is pressing the "a" key
+
+
+		if (Input.GetKey(KeyCode.A))  // If the player is pressing the "a" key
 		{
 			// Add a force to the left
 			Debug.Log("a pressed");
-			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 		}
 	}
 }
