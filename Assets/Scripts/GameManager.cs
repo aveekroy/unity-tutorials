acde9cb37a16e97e4317696b4b90f0803864ed84
Aveek Roy <aveek.roy@outlook.com>
2021-10-13 11:29:55 -0700

    added Restart method to restart the game when it collides with an obstacle or it falls off the ledge with an invoke method too

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
+    bool gameHasEnded=false;
+
     public void GameOver()
     {
-        Debug.Log("GAME OVER!!!");
+        if (gameHasEnded == false) { 
+            gameHasEnded = true;
+            Debug.Log("GAME OVER!!!");
+            // Invoke helps in delaying the function to be called - here 2 seconds delay in calling Restart function
+            Invoke("Restart", 2f);
+        }
+    }
+
+    void Restart()
+    {
+        //LoadScene - Loads the scene
+        //GetActiveScene - Gets the current scene of the game
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
